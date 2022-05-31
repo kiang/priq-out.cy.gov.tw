54440c22193a2f07dd902e7817b8835797d045dd
kiang <kiange@gmail.com>
2022-05-31 13:03:01 +0800

    commit only when needed

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -82,8 +82,10 @@ foreach ($lines as $line) {
     }
 }
 
-$now = date('Y-m-d H:i:s');
-exec("cd {$rootPath} && /usr/bin/git pull");
-exec("cd {$rootPath} && /usr/bin/git add -A");
-exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
-exec("cd {$rootPath} && /usr/bin/git push origin master");
+if ($downloadCounter > 0) {
+    $now = date('Y-m-d H:i:s');
+    exec("cd {$rootPath} && /usr/bin/git pull");
+    exec("cd {$rootPath} && /usr/bin/git add -A");
+    exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+    exec("cd {$rootPath} && /usr/bin/git push origin master");
+}
