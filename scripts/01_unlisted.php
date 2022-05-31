7d63f0be7c078c0260df42a40a9b994126622563
kiang <kiange@gmail.com>
2022-05-31 14:26:31 +0800

    add missing check to prevent duplicated download

--- scripts/01_unlisted.php
+++ scripts/01_unlisted.php
@@ -17,24 +17,50 @@ foreach (glob($rootPath . '/data/list/*/*.csv') as $csvFile) {
     }
 }
 
+$missingFile = $rootPath . '/raw/unlisted/missing.csv';
+if (file_exists($missingFile)) {
+    $fh = fopen($missingFile, 'r');
+    while ($line = fgets($fh, 512)) {
+        $pool[trim($line)] = true;
+    }
+}
+
 $downloadCounter = 0;
+$currentNo = 0;
 for ($i = 1; $i <= $max; $i++) {
+    $no = ceil($i / 500) * 500;
+    if ($currentNo !== $no) {
+        if ($currentNo > 0) {
+            $io = popen('/usr/bin/du -sk ' . $rootPath . '/raw/unlisted/' . $currentNo, 'r');
+            $size = fgets($io, 4096);
+            $size = substr($size, 0, strpos($size, "\t"));
+            pclose($io);
+            if ($size < 5) {
+                rmdir($rootPath . '/raw/unlisted/' . $currentNo);
+            }
+        }
+        $currentNo = $no;
+    }
     if (isset($pool[$i])) {
         continue;
     }
-    $no = ceil($i / 500) * 500;
     $filePath = $rootPath . '/raw/unlisted/' . $no;
     if (!file_exists($filePath)) {
         mkdir($filePath, 0777, true);
     }
+
     $targetFile = $filePath . '/' . $i . '.pdf';
     if (!file_exists($targetFile)) {
         $link = 'https://priq-out.cy.gov.tw/GipExtendWeb/wSite/SpecialPublication/fileDownload.jsp?id=' . $i;
         $c = file_get_contents($link);
         if (strlen($c) > 100) {
-            echo "{$targetFile} done\n";
             file_put_contents($targetFile, $c);
             ++$downloadCounter;
+            echo "[{$downloadCounter}]{$targetFile} done\n";
+        } else {
+            $oFh = fopen($missingFile, 'a+');
+            fputs($oFh, $i . "\n");
+            fclose($oFh);
         }
     }
     if ($downloadCounter === 500) {
